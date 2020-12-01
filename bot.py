9c71e738e372dd00ab3a45d50683ed59d226d66f
zoctobere <50931093+zoctobere@users.noreply.github.com>
2020-12-01 11:15:35 -0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+import discord
+import builtins
+import config
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+builtins.bot = bot
+
+import openSignups
+import getVolunteers
+import setVolunteers
+import publishAssignments
+import seedUsers
+import userNote
+
+@bot.event
+async def on_ready():
+    print('Beep boop. I am logged in and ready to go as {0.user}!'.format(bot))
+
+@bot.command()
+async def echo(ctx):
+    return
+
+bot.run(config.DISCORD_TOKEN)
